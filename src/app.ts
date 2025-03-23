0d20b322fe6ddbee3806955a791b9f7d5733ea55
c-jeongyyun <99jenny0614@naver.com>
2025-03-23 21:39:59 +0900

    👔 posting router 등록

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { addAuthRoutes } from "modules/auth/auth.controller";
 import "dotenv/config";
 
-import express from "express";
+import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 import { AppRouter } from "app.router";
 
 import cookieParser from "cookie-parser";
+import { addPostingRoutes } from "modules/posting/controller/posting.controller";
 
 const port = 8080;
 
@@ -21,6 +22,11 @@ const main = async (port: number) => {
 
   app.use(cookieParser());
 
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.sendStatus(500).send("Something broke!");
+  });
+
   // TODO 에러처리 이상함. 정상화필요 (첫번째 인자가 err가 아닌듯.. 쨌든 뭔가 이상함)
   // app.use((err: Error, req: Request, res: Response) => {
   //   console.log(err);
@@ -34,6 +40,7 @@ const main = async (port: number) => {
 
   // TODO 구조화할 방법 생각해보기 - addAuthRoutes ->  app.use(router); 순서가 유지되어야 하기 때문
   addAuthRoutes();
+  addPostingRoutes();
   app.use(router);
 };
 
